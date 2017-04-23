075cee947d4da01effa0663a809149ee8a5e02e4
Siddharth Sahay <sahaysid@gmail.com>
2017-04-23 22:01:40 +0100

    Grammar for user commands

--- grammar.py
+++ grammar.py
@@ -9,10 +9,16 @@ take_words = ["take", "pick", "lift"]
 drop_words = ["drop", "leave"]
 unlock_words = ["open", "unlock"]
 look_words = ["look", "see", "gaze"]
-
+object_wildcard = "object_"
+fighter_wildcard = "fighter_"
 
 what_next = ["What do you do? ", "What next? ", \
 		"What do you do next? "]
 
 grammar = [[[move_words], [direction_words]],\
-		[[take_words], ]
+		[[take_words], object_wildcard],\
+		[[fight_words], fighter_wildcard],\
+		[[drop_words], object_wildcard],\
+		[[unlock_words], object_wildcard],\
+		[[look_words]]
+		]
