c8785d88ecb794db6ad57b803b4c60cd11c210eb
Siddharth Sahay <sahaysid@gmail.com>
2017-05-03 15:55:05 +0530

    cx_Freeze build

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {"packages": ["json", "sets", "os", "pickle", "random", "sys", "nltk"]}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(  name = "zork",
+        version = "0.1",
+        keywords = "zork",
+        license = "Python Software Foundation License",
+        maintainer = "Siddharth Sahay",
+        maintainer_email = "sahaysid@gmail.com",
+        description = "Zork, the classic text based game, with a customisable map",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("zork.py", base=base)])
+
